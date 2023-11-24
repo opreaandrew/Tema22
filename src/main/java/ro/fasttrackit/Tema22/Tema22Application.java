c4c4e5206caedee82e42546986d49bad9ddd0944
Andrei Oprea <opreaandrew@yahoo.com>
2023-11-24 05:37:35 +0200

    Started

---
+++ src/main/java/ro/fasttrackit/Tema22/Tema22Application.java
@@ -0,0 +1,13 @@
+package ro.fasttrackit.Tema22;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Tema22Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Tema22Application.class, args);
+	}
+
+}
---
+++ src/test/java/ro/fasttrackit/Tema22/Tema22ApplicationTests.java
@@ -0,0 +1,13 @@
+package ro.fasttrackit.Tema22;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Tema22ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
